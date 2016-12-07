58d811334afb8f4a41547656cc65c38b536d3100
conorotompkins <conorotompkins@gmail.com>
2016-12-07 11:08:22 -0500

    got loess augment() to work. made graph

--- Graphs/NHL Coach graph attempts.R
+++ Graphs/NHL Coach graph attempts.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(broom)
+library(viridis)
 
 #load custom theme
 source("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/graphs/theme_nhh.R")
@@ -27,32 +28,29 @@ df <- team_data %>%
         filter(team %in% c("DET", "PIT"))
 #the problem is that the model thinks the data is 1732 rows long, and the dplyr pipe splits the data into two ~830 row sets
 model <- loess(CF.per ~ team_game_number, data = df, span = .15)
-class(model)
-head(augment(model, df))
 
-df <- df %>%
-        select(team, team_game_number, head_coach_u, CF.per) %>%
-        group_by(team) %>%
-        do(fit =tidy(augment(model, .)))
-?tidy
-ggplot(df, aes(team_game_number, .fitted)) +
+
+
+?augment
+
+team_data <- team_data %>% 
+        group_by(franchise_name) %>% 
+        do(augment(loess(CF.per ~ team_game_number, span = .15, data = .), newdata = .))
+
+ggplot(team_data, aes(team_game_number, .fitted)) +
         geom_hline(yintercept = 50, size = .25, alpha = I(1)) +
         geom_vline(xintercept = lines, size = .25, alpha = I(1)) +
         geom_ribbon(aes(ymax = (.fitted + 1.96 * .se.fit), ymin = (.fitted - 1.96 * .se.fit), fill = head_coach_u), alpha = I(.5)) +
         geom_line(aes(color = head_coach_u), size = 2) +
         scale_x_continuous(breaks = lines, labels = seasons) +
-        facet_wrap(~team, ncol = 1) +
-        #scale_color_discrete(name = paste(franchise, "Head Coaches"), 
-                             #breaks = unique(team_data$head_coach_u[team_data$franchise_name == franchise]),
-                             #labels = unique(team_data$head_coach_u[team_data$franchise_name == franchise])) +
+        facet_wrap(~franchise_name, ncol = 6) +
+        scale_color_viridis(discrete = TRUE) +
+        scale_fill_viridis(discrete = TRUE) +
         coord_cartesian(ylim = c(40, 60)) +
         labs(y = "5v5 Shots For %", 
              x = "Season",
              title = "NHL Head Coaches Historical View, 2005-2016") +
-        guides(fill = FALSE)
-        
-df
-
-df <- df %>%
-        tidy(fit)
-df
+        guides(fill = FALSE, color = FALSE) +
+        theme(panel.grid.major = element_blank(), 
+              axis.text.x = element_text(size = 10))
+ggsave("NHL coaches big graph.png")
