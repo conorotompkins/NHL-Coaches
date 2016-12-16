32833b7dc3eb398916d8566f047fe7cf1c864897
conorotompkins <conorotompkins@gmail.com>
2016-12-15 19:25:56 -0500

    cleaned up code

--- Graphs/NHL Coach graph code.R
+++ Graphs/NHL Coach graph code.R
@@ -1,15 +1,12 @@
-library(ggthemes)
-library(scales)
-library(ggplot2)
-library(dplyr)
-library(lubridate)
-library(stringr)
-library(scales)
-library(readr)
-library(tidyr)
-library(cowplot)
+setwd("C:/Users/conor/githubfolder/NHL-Coaches/Graphs")
 
-team_data <- read_csv("https://raw.githubusercontent.com/conorotompkins/NHL-Coaches/master/NHH_coach_data.csv")
+library(tidyverse)
+library(broom)
+library(viridis)
+library(ggtree)
+library(curl)
+
+league_data <- read_csv("https://raw.githubusercontent.com/conorotompkins/NHL-Coaches/master/NHH_coach_data.csv")
 
 #load custom theme
 source("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/graphs/theme_nhh.R")
@@ -29,41 +26,62 @@ seasons <- c("06-07",
              "14-15",
              "15-16")
 
-#set graph criteria
-team <- "DET"
-franchise <- "Detroit Red Wings"
+#big graph
+df <- league_data %>% 
+        group_by(franchise_name) %>% 
+        do(augment(loess(CF.per ~ team_game_number, span = .15, data = .), newdata = .))
 
-DET_team <- team_data %>%
-        filter(team == "DET")
-
-#create statistical model
-fit <- loess(DET_team$CF.per ~ DET_team$team_game_number, span = .15)
-
-pred <- predict(fit, se=TRUE)
-
-DET_team$fit <- pred$fit
-DET_team$se.fit <- pred$se.fit
-fit <- NULL
-
-#create plot
-det_plot <- ggplot(DET_team, aes(x = team_game_number, y = fit)) +
+ggplot(df, aes(team_game_number, .fitted)) +
         geom_hline(yintercept = 50, size = .25, alpha = I(1)) +
         geom_vline(xintercept = lines, size = .25, alpha = I(1)) +
-        geom_ribbon(aes(ymax = (fit + 1.96 * se.fit), ymin = (fit - 1.96 * se.fit), fill = head_coach_u), alpha = I(.5)) +
+        geom_ribbon(aes(ymax = (.fitted + 1.96 * .se.fit), ymin = (.fitted - 1.96 * .se.fit), fill = head_coach_u), alpha = I(.5)) +
         geom_line(aes(color = head_coach_u), size = 2) +
         scale_x_continuous(breaks = lines, labels = seasons) +
-        scale_color_discrete(name = paste(franchise, "Head Coaches"), 
-                             breaks = unique(team_data$head_coach_u[team_data$franchise_name == franchise]),
-                             labels = unique(team_data$head_coach_u[team_data$franchise_name == franchise])) +
+        facet_wrap(~franchise_name, ncol = 6) +
+        scale_color_viridis(discrete = TRUE) +
+        scale_fill_viridis(discrete = TRUE) +
         coord_cartesian(ylim = c(40, 60)) +
         labs(y = "5v5 Shots For %", 
              x = "Season",
              title = "NHL Head Coaches Historical View, 2005-2016") +
-        guides(fill = FALSE) +
-        theme_nhh() +
-        theme(panel.grid = element_blank(), 
-              legend.position = "top")
-print(det_plot)
+        guides(fill = FALSE, color = FALSE) +
+        theme(panel.grid.major = element_blank(), 
+              axis.text.x = element_text(size = 10))
+ggsave("NHL coaches big graph.png", height = 12, width = 45)
 
+#set graph criteria
+set_franchise <- "Washington Capitals"
+
+df <- league_data %>%
+        filter(franchise_name == set_franchise) %>%
+        do(augment(loess(CF.per ~ team_game_number, span = .15, data = .), newdata = .))
+        
+#create plot
+ggplot(df, aes(team_game_number, .fitted)) +
+        geom_hline(yintercept = 50, size = .25, alpha = I(1)) +
+        geom_vline(xintercept = lines, size = .25, alpha = I(1)) +
+        geom_ribbon(aes(ymax = (.fitted + 1.96 * .se.fit), ymin = (.fitted - 1.96 * .se.fit), fill = head_coach_u), alpha = I(.5)) +
+        geom_line(aes(color = head_coach_u), size = 2) +
+        facet_wrap(~franchise_name, ncol = 6) +
+        coord_cartesian(ylim = c(40, 60)) +
+        scale_x_continuous(breaks = lines, labels = seasons) +
+        #scale_color_discrete(name = paste(set_franchise, "Head Coaches"), 
+        # breaks = unique(df$head_coach_u[df$franchise_name == set_franchise]),
+        #labels = unique(df$head_coach_u[df$franchise_name == set_franchise])) +
+        scale_color_viridis(discrete = TRUE, name = "Head Coaches", 
+                breaks = unique(df$head_coach_u[df$franchise_name == set_franchise]),
+                labels = unique(df$head_coach_u[df$franchise_name == set_franchise]),
+                direction = -1) +
+        scale_fill_viridis(discrete = TRUE,
+                                   direction = -1) +
+        labs(y = "5v5 Shots For %", 
+                     x = "Season",
+                     title = "NHL Head Coaches Historical View, 2005-2016") +
+        guides(fill = FALSE) +
+        theme(panel.grid.major = element_blank(), 
+              axis.text.x = element_text(size = 10))
+        
 #save plot
-ggsave(paste(franchise, "coach graph.png"), width = 15, height = 9)
+ggsave(paste(set_franchise, "coach graph.png"), width = 15, height = 9)
+
+
